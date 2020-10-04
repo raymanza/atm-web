29c0b4f17f7ca373fdcfdd5d118a0d0a225952dc
6110402800 <naran.m@ku.th>
2020-10-04 22:48:25 +0700

    add customer bankaccount feature

--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -1,27 +1,42 @@
 package th.ac.ku.atm.service;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import th.ac.ku.atm.model.BankAccount;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class BankAccountService {
-    private List<BankAccount> bankAccountList;
 
-    @PostConstruct
-    public void postConstruct() {
-        this.bankAccountList = new ArrayList<>();
-    }
-    public void createBankAccount(BankAccount bankAccount) {
-        bankAccountList.add(bankAccount);
+    private RestTemplate restTemplate;
+
+    public BankAccountService(RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
     }
 
-    public List<BankAccount> getBankAccounts() {
-        return new ArrayList<>(bankAccountList);
+    public List<BankAccount> getCustomerBankAccounts(int customerId){
+
+        String url = "http://localhost:8091/api/bankaccount/customer/" + customerId;
+
+        ResponseEntity<BankAccount[]> response =
+                restTemplate.getForEntity(url, BankAccount[].class);
+        BankAccount[] accounts = response.getBody();
+        return Arrays.asList(accounts);
     }
+
+
+
+
+
+
+//    public List<BankAccount> getBankAccounts() {
+//        return new ArrayList<>(bankAccountList);
+//    }
 
 }
